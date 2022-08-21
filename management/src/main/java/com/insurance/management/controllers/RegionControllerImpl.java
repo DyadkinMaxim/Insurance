516107a15cf1e9ab0afa8a1546b76759ed5f2541
Maxim Dyadkin <dyadkinm@gmail.com>
2022-08-21 18:50:25 +0300

    insurance-simple: upload fixes

--- management/src/main/java/com/insurance/management/controllers/RegionControllerImpl.java
+++ management/src/main/java/com/insurance/management/controllers/RegionControllerImpl.java
@@ -1,9 +1,9 @@
 package com.insurance.management.controllers;
 
 import com.insurance.management.dto.RegionDTO;
-import com.insurance.management.repository.RegionRepository;
 import com.insurance.management.service.RegionService;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -17,8 +17,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -51,7 +49,10 @@ public class RegionControllerImpl implements RegionController {
         return actual;
     }
 
-    @PostMapping("/management/region/newCSV")
+    @PostMapping(value = "/management/region/newCSV",
+            consumes = {MediaType.MULTIPART_FORM_DATA_VALUE},
+            produces = {MediaType.APPLICATION_JSON_VALUE}
+            )
     public ResponseEntity<ResponseMessage> uploadFile(@RequestParam("file") MultipartFile file) {
         String message = "";
 
